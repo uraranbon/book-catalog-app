141c00486ae7cfa4c02a4d1320307898ea1b8b79
enomoto <uraraenomoto@gmail.com>
2023-09-10 21:20:59 +0900

    router

---
+++ src/routes/Router.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Detail } from "../pages/Detail";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
