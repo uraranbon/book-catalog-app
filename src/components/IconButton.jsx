0862173fdb913a1d1be22e6bbe30d37a029ee7f3
enomoto <uraraenomoto@gmail.com>
2023-09-10 23:26:14 +0900

    ダークモード

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import styles from "./IconButton.module.scss";
 
+const isDarkModeEnabled = () => {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+};
+
 const IconButton = ({ image, text }) => {
+  //ダークモード判別
+  const isDark = isDarkModeEnabled();
+  const imagePath = isDark
+    ? `/image/icon/dark/icon_study_${image}.svg`
+    : `/image/icon/light/icon_study_${image}.svg`;
+
   return (
     <button className={styles.button}>
       <div className={styles.icon}>
-        <img src={`/image/icon/light/icon_study_${image}.svg`} alt="" />
+        <img src={imagePath} alt="" />
       </div>
       <span className={styles.text}>{text}</span>
     </button>
